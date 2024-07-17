745832249a4ee5f7e05c697a7b99fa2d4efdc483
Thabiso Pheko <tabs150@gmail.com>
2024-07-17 18:25:21 +0200

    Tests for the Togglable component

--- src/components/Note.test.jsx
+++ src/components/Note.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Note from "./Note";
-import { expect } from "vitest";
 
 test('renders content', () => {
   const note = {
@@ -12,4 +12,21 @@ test('renders content', () => {
 
   const element = screen.getByText('Component testing is done with react-testing-library')
   expect(element).toBeDefined()
+
+})
+test('clicking the button calls event handler once', async () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+
+  const mockHandler = vi.fn()
+
+  render(<Note note={note} toggleImportance={mockHandler} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('make not important')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
